dad5619e0147c25f01f9a45ecd917c3275e80cb8
Sunil Musinada <sunil.chaitanya@gmail.com>
2019-01-30 13:20:28 +0530

    Updates

---
+++ precache-manifest.66afb1892efd1eb9be99bf0626139c41.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Vidly-1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e3ad60042e15f03aa6c9",
+    "url": "/Vidly-1/static/css/main.c0458f96.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Vidly-1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fdbd3119ef0fb86f7469",
+    "url": "/Vidly-1/static/js/1.fdbd3119.chunk.js"
+  },
+  {
+    "revision": "6a30dcf8a198fef0cffc",
+    "url": "/Vidly-1/static/js/runtime~main.6a30dcf8.js"
+  },
+  {
+    "revision": "e3ad60042e15f03aa6c9",
+    "url": "/Vidly-1/static/js/main.e3ad6004.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Vidly-1/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Vidly-1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Vidly-1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fdbd3119ef0fb86f7469",
+    "url": "/Vidly-1/static/css/1.4d04194c.chunk.css"
+  },
+  {
+    "revision": "09d166683d475358c4a5833d168a2a93",
+    "url": "/Vidly-1/index.html"
+  }
+];
